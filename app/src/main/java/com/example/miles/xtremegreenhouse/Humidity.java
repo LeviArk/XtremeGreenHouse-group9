326f8b655325015dcc5fada9cd6a01c4bdf30e6c
Ark <digitalshadow2.0@gmail.com>
2017-06-01 12:17:37 -0300

    Starting The Humidity Like a Beach Branch

---
+++ app/src/main/java/com/example/miles/xtremegreenhouse/Humidity.java
@@ -0,0 +1,14 @@
+package com.example.miles.xtremegreenhouse;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ark on 2017-06-01.
+ * AND ONLY BY ARK. Connor seems to be missing again.
+ */
+
+public class Humidity extends AppCompatActivity {
+
+    //stuff
+
+}
